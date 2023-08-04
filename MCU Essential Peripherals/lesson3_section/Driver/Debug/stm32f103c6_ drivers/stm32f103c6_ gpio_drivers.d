f0d04225ef4f6d2e0f18b0b1b9d438d0ebc4d644
Ahmed shaban mohammed <109414595+ahmedshaban744@users.noreply.github.com>
2023-08-04 18:54:49 +0300

    Delete MCU Essential Peripherals/lesson3_section/Driver directory

--- MCU Essential Peripherals/lesson3_section/Driver/Debug/stm32f103c6_ drivers/stm32f103c6_ gpio_drivers.d
+++
@@ -1,8 +0,0 @@
-stm32f103c6_ drivers/stm32f103c6_ gpio_drivers.o: \
- ../stm32f103c6_\ drivers/stm32f103c6_\ gpio_drivers.c \
- C:/Users/ahmed/Desktop/stm\ project/stm32/Driver/stm32f103c6_\ drivers/inc/stm32f103c6_\ gpio_drivers.h \
- C:/Users/ahmed/Desktop/stm\ project/stm32/Driver/stm32f103c6_\ drivers/inc/stm32f103x6.h
-
-C:/Users/ahmed/Desktop/stm\ project/stm32/Driver/stm32f103c6_\ drivers/inc/stm32f103c6_\ gpio_drivers.h:
-
-C:/Users/ahmed/Desktop/stm\ project/stm32/Driver/stm32f103c6_\ drivers/inc/stm32f103x6.h:
