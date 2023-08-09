a652daa5bbeaa456fa1cd8ec72af33566b1282a5
Ahmed shaban mohammed <109414595+ahmedshaban744@users.noreply.github.com>
2023-08-09 22:54:38 +0300

    Delete MCU Essential Peripherals/lesson 4 EXTI directory

--- MCU Essential Peripherals/lesson 4 EXTI/Driver/Debug/stm32f103c6_ drivers/GPIO/stm32f103c6_GPIO_drivers.d
+++
@@ -1,8 +0,0 @@
-stm32f103c6_ drivers/GPIO/stm32f103c6_GPIO_drivers.o: \
- ../stm32f103c6_\ drivers/GPIO/stm32f103c6_GPIO_drivers.c \
- C:/Users/ahmed/Desktop/stm\ project/stm32/Driver/stm32f103c6_\ drivers/inc/stm32f103c6_GPIO_drivers.h \
- C:/Users/ahmed/Desktop/stm\ project/stm32/Driver/stm32f103c6_\ drivers/inc/stm32f103x6.h
-
-C:/Users/ahmed/Desktop/stm\ project/stm32/Driver/stm32f103c6_\ drivers/inc/stm32f103c6_GPIO_drivers.h:
-
-C:/Users/ahmed/Desktop/stm\ project/stm32/Driver/stm32f103c6_\ drivers/inc/stm32f103x6.h:
